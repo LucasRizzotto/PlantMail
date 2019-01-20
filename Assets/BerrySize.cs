a94f889550eedd94b1f92374fe86546973a3f195
Lucas Rizzotto <contact@lucasrizzotto.com>
2019-01-19 23:39:20 -0800

    Basic animations for folding/unfolding of the berries

---
+++ Assets/BerrySize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BerrySize : MonoBehaviour {
+
+    [Range(1f, 2f)]
+    public float SizeMultiplier = 1f;
+
+    private Vector3 InitialScale;
+
+    public void Start()
+    {
+        InitialScale = transform.localScale;
+        UpdateSize();
+    }
+
+    void UpdateSize()
+    {
+        transform.localScale = InitialScale * SizeMultiplier;
+    }
+
+}
---
+++ Assets/EmailUnfold.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EmailUnfold : MonoBehaviour {
+
+    public Animator EmailAnimator;
+    [Space(5)]
+    public KeyCode FoldDebugKey = KeyCode.F;
+    public string UnfoldEmailTrigger = "";
+    public string FoldEmailTrigger = "";
+
+    private bool Folded = true;
+
+    void Start () {
+        EmailAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(FoldDebugKey))
+        {
+            ToggleFolding();
+        }
+    }
+
+    public void ToggleFolding()
+    {
+        if(Folded)
+        {
+            Unfold();
+        }
+        else
+        {
+            Fold();
+        }
+    }
+
+    void Unfold()
+    {
+        Debug.Log("Unfolding e-mail...");
+        EmailAnimator.SetTrigger(UnfoldEmailTrigger);
+        Folded = false;
+    }
+
+    void Fold()
+    {
+        Debug.Log("Folding back e-mail...");
+        EmailAnimator.SetTrigger(FoldEmailTrigger);
+        Folded = true;
+    }
+
+}
