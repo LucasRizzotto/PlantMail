3bb616ec5a1a8ffcd78eba51623f269e77ec7cae
Lucas Rizzotto <contact@lucasrizzotto.com>
2019-01-19 22:12:54 -0800

    Basic pot

---
+++ Assets/PotName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PotName : MonoBehaviour {
+
+    public string Name = "";
+    public TextMeshPro Text;
+
+    void Update () {
+        Text.text = Name;
+	}
+}
