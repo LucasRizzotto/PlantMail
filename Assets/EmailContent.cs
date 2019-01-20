f512e05fa4ae90184048644e2cc461d92b4ac670
Lucas Rizzotto <contact@lucasrizzotto.com>
2019-01-19 23:51:47 -0800

    Added script for updating the content of e-mails

---
+++ Assets/EmailContent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EmailContent : MonoBehaviour {
+
+    public string From = "Skippy Dinglechalk";
+    public string To = "Bambalam Zengleworth";
+    public string Subject = "Who let the dogs out?";
+    [TextArea(5, 10)]
+    public string Body = "Donec efficitur id eros a fermentum. Duis neque erat, facilisis sit amet rutrum ut, ultrices eu quam. Nam id sodales nunc. Etiam eu condimentum libero. In pharetra faucibus mauris, sit amet tempus dui blandit quis. Fusce dignissim lectus quis diam finibus aliquam. Cras ac tellus tempus, efficitur libero et, rutrum metus. Nunc imperdiet ultricies tempus. Duis at vehicula neque. Pellentesque aliquet at orci at dignissim. Aenean tincidunt fringilla accumsan. Praesent rhoncus eros urna, nec accumsan diam facilisis sit amet. Integer a finibus mauris, nec consectetur ante.";
+
+    [Space(10)]
+    public TextMeshPro FromTMP;
+    public TextMeshPro ToTMP;
+    public TextMeshPro SubjectTMP;
+    public TextMeshPro BodyTMP;
+	
+	void Update () {
+        UpdateContentOfEmail();
+	}
+
+    void UpdateContentOfEmail()
+    {
+        FromTMP.text = From;
+        ToTMP.text = To;
+        SubjectTMP.text = Subject;
+        BodyTMP.text = Body;
+    }
+}
